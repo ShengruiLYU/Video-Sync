acbae4654bdb59d08e4432b56ba647242c1e5736
ShengruiLYU <slvaa@connect.ust.hk>
2018-04-03 01:19:22 -0400

    Add new script for process the .gz files
    unzip files into the same directory as compressed files

---
+++ processzipfile.py
@@ -0,0 +1,22 @@
+import gzip
+import glob, os
+
+
+path = "C:/Users/Shengrui/Downloads/Trial1/Wearable_Data/files/raw/"
+
+
+
+
+for counter, file in enumerate(glob.glob(path+"*/*.gz")):
+   if os.path.isdir(file) == False:
+        outputfile = file[:-3]
+        
+
+        # uncompress the file
+        inF = gzip.open(file, 'rb')
+        outF = open( outputfile , 'wb')
+        outF.write( inF.read() )
+        inF.close()
+        outF.close()
+
+    
